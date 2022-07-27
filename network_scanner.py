8516d89c2640ba2b41a62a65971fe2230850ba63
knhn1004 <kunhan945@gmail.com>
2022-07-27 13:43:10 -0400

    improving program output

--- network_scanner.py
+++ network_scanner.py
@@ -6,10 +6,10 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast / arp_request
-    answered_list, _ = scapy.srp(arp_request_broadcast, timeout=1)
-    for e in answered_list:
-        print(e[1].psrc)
-        print(e[1].hwsrc)
-        print('-'*80)
+    answered_list, _ = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+
+    print('IP\t\t\t\tMAC_ADDRESS')
+    for ele in answered_list:
+        print(ele[1].psrc + '\t' * 3 + ele[1].hwsrc)
 
 scan('10.0.2.2/24')
