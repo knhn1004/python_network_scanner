a7d9df4dc06e1f2b560f39d5b284ec734a7c0edc
knhn1004 <kunhan945@gmail.com>
2022-07-27 13:11:25 -0400

    using scapy to create an arp request combining framews to broadcast packets

---
+++ network_scanner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    broadcast.show()
+
+    arp_request_broadcast = broadcast / arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
+
+scan('10.0.2.2/24')
