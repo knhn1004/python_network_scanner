f6600097cabb5699c4c7ec67e743ae4abb7dc9eb
knhn1004 <kunhan945@gmail.com>
2022-07-27 13:25:14 -0400

    sending and receiving packets

--- network_scanner.py
+++ network_scanner.py
@@ -4,13 +4,9 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
-
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    broadcast.show()
-
     arp_request_broadcast = broadcast / arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered, _ = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan('10.0.2.2/24')
