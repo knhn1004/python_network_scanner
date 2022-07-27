cd2633bc73050ab9ba02e9f1be1cfffcb2d94193
knhn1004 <kunhan945@gmail.com>
2022-07-27 13:38:20 -0400

    iterating list showing ip and mac addresses

--- network_scanner.py
+++ network_scanner.py
@@ -6,7 +6,10 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast / arp_request
-    answered, _ = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered_list, _ = scapy.srp(arp_request_broadcast, timeout=1)
+    for e in answered_list:
+        print(e[1].psrc)
+        print(e[1].hwsrc)
+        print('-'*80)
 
 scan('10.0.2.2/24')
